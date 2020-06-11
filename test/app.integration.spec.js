99e47ba5d93c3b58759f2ee4942236685f19a537
Justine810 <justine.gautreau@gmail.com>
2020-06-11 17:51:55 +0200

    tous les tests passent

--- test/app.integration.spec.js
+++ test/app.integration.spec.js
@@ -1,8 +1,12 @@
 // test/app.integration.spec.js
 const request = require('supertest');
 const app = require('../app');
+const connection = require('../connection');
+
 
 describe('Test routes', () => {
+    // truncate bookmark table before each test
+    beforeEach(done => connection.query('TRUNCATE bookmark', done));
   it('GET / sends "Hello World" as json', (done) => {
     request(app)
       .get('/')
@@ -26,6 +30,30 @@ describe('Test routes', () => {
         expect(response.body).toEqual(expected);
         done();
       });
-  })
+    });
 
+      it('POST /bookmarks - OK (fields provided', (done) => {
+        request(app)
+        .post('/bookmarks')
+        .send ({url: 'https://jestjs.io', title: 'Jest'})
+        .expect(201)
+        .expect('Content-Type', /json/)
+        .then(response => {
+            const expected = { id: expect.any(Number), url: 'https://jestjs.io', title: 'Jest' };
+          expect(response.body).toEqual(expected);
+          done();
+        })
+        .catch(done); 
+  });
 });
+
+describe('GET /bookmarks/:id', () => {
+    const testBookmark = { url: 'https://nodejs.org/', title: 'Node.js' };
+    beforeEach((done) => connection.query(
+      'TRUNCATE bookmark', () => connection.query(
+        'INSERT INTO bookmark SET ?', testBookmark, done
+      )
+    ));
+  
+    // Write your tests HERE!
+  });
